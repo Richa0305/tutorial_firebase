645671fc2166f68b09e15cc0c89fc400c1e47e7f
Richa0305 <richa0305@gmail.com>
2021-11-05 17:34:21 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List todos = List.empty();
+  String title = "";
+  String description = "";
+  @override
+  void initState() {
+    super.initState();
+    todos = ["Hello", "Hey There"];
+  }
+
+  createToDo() {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection("MyTodos").doc(title);
+
+    Map<String, String> todoList = {
+      "todoTitle": title,
+      "todoDesc": description
+    };
+
+    documentReference
+        .set(todoList)
+        .whenComplete(() => print("Data stored successfully"));
+  }
+
+  deleteTodo(item) {
+
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection("MyTodos").doc(item);
+
+        documentReference.delete().whenComplete(() => print("deleted successfully"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          } else if (snapshot.hasData || snapshot.data != null) {
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  QueryDocumentSnapshot<Object?>? documentSnapshot =
+                      snapshot.data?.docs[index];
+                  return Dismissible(
+                      key: Key(index.toString()),
+                      child: Card(
+                        elevation: 4,
+                        child: ListTile(
+                          title: Text((documentSnapshot != null) ? (documentSnapshot["todoTitle"]) : ""),
+                          subtitle: Text((documentSnapshot != null)
+                              ? ((documentSnapshot["todoDesc"] != null)
+                                  ? documentSnapshot["todoDesc"]
+                                  : "")
+                              : ""),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            color: Colors.red,
+                            onPressed: () {
+                              setState(() {
+                                //todos.removeAt(index);
+                                deleteTodo((documentSnapshot != null) ? (documentSnapshot["todoTitle"]) : "");
+                              });
+                            },
+                          ),
+                        ),
+                      ));
+                });
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                Colors.red,
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  title: const Text("Add Todo"),
+                  content: Container(
+                    width: 400,
+                    height: 100,
+                    child: Column(
+                      children: [
+                        TextField(
+                          onChanged: (String value) {
+                            title = value;
+                          },
+                        ),
+                        TextField(
+                          onChanged: (String value) {
+                            description = value;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            //todos.add(title);
+                            createToDo();
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("Add"))
+                  ],
+                );
+              });
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
